398bf4bbf8bd8abdfcc24a7add1d00134f1f7644
Yasin <yasinabdussaburr@gmail.com>
2021-07-14 17:30:00 -0400

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { image } from "../data/data";
+import NavBar from "./NavBar";
 
 function About() {
-  return <div>About</div>;
+  <NavBar/>
+  return <div id='about'>
+    <h2>About Me</h2>
+    <p>This supposed to be the about section</p>
+    <img src={image} alt='I made this'></img>
+  </div>;
+
 }
 
 export default About;
